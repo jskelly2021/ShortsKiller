17de532e804e1a9a18ff2860593aad7829de7f48
Jacob Kelly <jskelly2021@gmail.com>
2026-02-21 15:39:53 -0600

    Initial Commit

---
+++ ShortsKillerTests/ShortsKillerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShortsKillerTests.swift
+//  ShortsKillerTests
+//
+//  Created by Jacob Kelly on 2/21/26.
+//
+
+import Testing
+@testable import ShortsKiller
+
+struct ShortsKillerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
