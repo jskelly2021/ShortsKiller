b7f1e0efd75381a629dc07d90913f6ce47934748
Jacob Kelly <jskelly2021@gmail.com>
2026-03-12 01:27:11 -0500

    Added a simple redirect back to homepage if shorts is found in url path

--- ShortsKiller Extension/Resources/content.js
+++ ShortsKiller Extension/Resources/content.js
@@ -1,1 +1,15 @@
 
+const REDIRECT_URL = "https://www.youtube.com/";
+
+function isShortsPage() {
+    const segments = window.location.pathname.split("/").filter(Boolean);
+    return segments.includes("shorts");
+}
+
+function redirectIfShorts() {
+    if (isShortsPage() && window.location.href !== REDIRECT_URL) {
+        window.location.replace(REDIRECT_URL);
+    }
+}
+
+redirectIfShorts();
