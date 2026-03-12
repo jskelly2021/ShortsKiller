fce91357745add5796723a646d5e6769d45d4b58
Jacob Kelly <jskelly2021@gmail.com>
2026-03-12 00:42:20 -0500

    Converted app to use SwiftUI instead of UIKit

---
+++ ShortsKiller/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  ShortsKiller
+//
+//  Created by Jacob Kelly on 3/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Image("AppLogo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 120)
+
+            Text("ShortsKiller")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Text("Hides YouTube Shorts content in Safari.")
+                .multilineTextAlignment(.center)
+
+            VStack(alignment: .leading, spacing: 10) {
+                Text("How to enable:")
+                    .font(.headline)
+
+                Text("1. Open Settings")
+                Text("2. Go to Safari")
+                Text("3. Tap Extensions")
+                Text("4. Enable ShortsKiller")
+                Text("5. Allow it on YouTube")
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding()
+            .background(.thinMaterial)
+            .cornerRadius(12)
+        }
+        .padding()
+    }
+}
---
+++ ShortsKiller/ShortsKillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortsKillerApp.swift
+//  ShortsKiller
+//
+//  Created by Jacob Kelly on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ShortsKillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
